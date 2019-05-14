32fc26b075b049028142a4ecb1c76aff73c00e6c
TANUT NAMCOT <tanut_namcot@cmu.ac.th>
2019-05-14 12:52:16 +0700

    Add angular for following

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -8,7 +8,20 @@ angular.module('followingList', ['ngRoute'])
 
       const requestOptions = {
           headers: { 'X-session': $rootScope.x_session }
-      };
+        };
+
+        self.getfollow = function getfollow(username)
+        {
+
+            const data = "peoplefollowing=" + encodeURIComponent(username);
+            $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions);
+        }
+        self.getunfollow = function getunfollow(username)
+        {
+            $http.defaults.headers.delete = { 'X-session': $rootScope.x_session };
+            const data = "peoplefollowing=" + encodeURIComponent(username);
+            $http.delete('http://localhost:8080/twitterapi/following/?' + data);
+        }
 
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
